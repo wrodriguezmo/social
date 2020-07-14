7e79f9d2a6b08e097bdb088359e16052d126ddeb
William Rodríguez Mora <wrodriguezmo@gmail.com>
2020-07-14 16:35:48 -0500

    Inicial en JS

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore'
 
 // firebase init
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: "AIzaSyDSJ0oVCVFn33hsRP66o8CDKS5pnikP4ao",
+  authDomain: "calendar-a41be.firebaseapp.com",
+  databaseURL: "https://calendar-a41be.firebaseio.com",
+  projectId: "calendar-a41be",
+  storageBucket: "calendar-a41be.appspot.com",
+  messagingSenderId: "446517952910",
+  appId: "1:446517952910:web:352ecdc029a245849ee4df"
 }
 firebase.initializeApp(firebaseConfig)
 
